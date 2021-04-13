9404f31f36af18ba241e7ab7302f6b5c49243544
Sidharth Soman <siddu.soman65@gmail.com>
2021-04-13 20:32:51 +0530

    new problem, array of 1s and 0s

--- slidingwindow/class2.js
+++ slidingwindow/class2.js
@@ -1,6 +1,6 @@
 //longest substring with repeating character allowed k replacements
 //i aint understanding shit :(
-
+//abbcb k = 1 (one replacement) a: 1, b: 2 , repeatCount=2
 function longestSubstring(str, k) {
     let windowStart = 0, windowEnd = 0, maxRepeatCount = 0, freqMap = {}, maxLength = 0;
     for(windowEnd=0;windowEnd<str.length;windowEnd++) {
@@ -19,4 +19,23 @@ function longestSubstring(str, k) {
         maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
     }
     return maxLength;
-}
+} // O(n) time | O(1) space where n is the number of letters in the input string
+
+//longest subarray with repeating ones (allowed k replacements of 0)
+
+function lengthLongestSubarray(arr, k) {
+    let windowStart = 0, windowEnd = 0, maxOnes = 0;
+    for(windowEnd=0;windowEnd<arr.length;windowEnd++) {
+        if(arr[windowEnd] == 1) {
+            maxOnes++;
+        }
+        if(windowEnd - windowStart + 1 - maxOnes > k) {
+            if(arr[windowStart] == 1){
+                maxOnes -= 1;
+            }
+            windowStart++;
+        }
+        maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+    }
+    return maxLength;
+} //O(n) time | O(1) space where n is the number of letters in the input string
